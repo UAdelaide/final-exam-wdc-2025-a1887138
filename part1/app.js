0ddec3d5f924f7c9b1be9d5e076125a2ca89ca84
a1887138 <a1887138@adelaide.edu.au>
2025-06-20 07:59:49 +0000

    [automatic commit] 2025-06-20 07:59:49 UTC 101.115.236.113

--- part1/app.js
+++ part1/app.js
@@ -201,7 +201,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
 });
 
 // Route to return a summary of walkers as JSON
-app.get('/api/walkrequests/open', async (req, res) => {
+app.get('/api/walkers/summary', async (req, res) => {
 
   try {
     const [walkers] = await db.execute(`
