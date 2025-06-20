4f5c1eb684d72d5d02bd363cddc74f16018e7fce
a1887138 <a1887138@adelaide.edu.au>
2025-06-20 05:49:18 +0000

    [automatic commit] 2025-06-20 05:49:18 UTC 101.115.236.113

--- part1/app.js
+++ part1/app.js
@@ -128,8 +128,8 @@ app.get('/api/walkrequests/open', async (req, res) => {
     const [dogs] = await db.execute(`
       SELECT WalkRequests.request_id, Dogs.name, WalkRequests.requested_time, WalkRequests.duration, WalkRequests.location,
       Users.username;
-      FROM WalkRequests
-      INNER JOIN Dogs ON WalkRequests.dog_id = Dogs.
+      FROM ((WalkRequests
+      INNER JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
 
       WHERE Status = 'open'
       `);
