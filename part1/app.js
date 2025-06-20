cf96851978596750108e0e7c1bd852041a0d0373
a1887138 <a1887138@adelaide.edu.au>
2025-06-20 06:39:58 +0000

    [automatic commit] 2025-06-20 06:39:58 UTC 101.115.236.113

--- part1/app.js
+++ part1/app.js
@@ -78,16 +78,20 @@ let db;
         CONSTRAINT unique_application UNIQUE (request_id, walker_id)
         );
       `);
+
       await db.execute(`
-        CREATE TABLE WalkApplications (
-        application_id INT AUTO_INCREMENT PRIMARY KEY,
+        CREATE TABLE WalkRatings (
+        rating_id INT AUTO_INCREMENT PRIMARY KEY,
         request_id INT NOT NULL,
         walker_id INT NOT NULL,
-        applied_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        status ENUM('pending', 'accepted', 'rejected') DEFAULT 'pending',
+        owner_id INT NOT NULL,
+        rating INT CHECK (rating BETWEEN 1 AND 5),
+        comments TEXT,
+        rated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         FOREIGN KEY (request_id) REFERENCES WalkRequests(request_id),
         FOREIGN KEY (walker_id) REFERENCES Users(user_id),
-        CONSTRAINT unique_application UNIQUE (request_id, walker_id)
+        FOREIGN KEY (owner_id) REFERENCES Users(user_id),
+        CONSTRAINT unique_rating_per_walk UNIQUE (request_id)
         );
       `);
 
