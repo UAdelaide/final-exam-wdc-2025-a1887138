85d6fc0e8804389a25919721ff189d845ac89003
a1887138 <a1887138@adelaide.edu.au>
2025-06-20 03:54:43 +0000

    [automatic commit] 2025-06-20 03:54:43 UTC 101.115.236.113

--- part1/app.js
+++ part1/app.js
@@ -19,36 +19,36 @@ let db;
 
 (async() => {
 
-    const connection = mysql.createConnection({
-    host: 'localhost'
-    });
+        const connection = mysql.createConnection({
+        host: 'localhost'
+        });
 
-    await connection.query('CREATE DATABASE IF NOT EXISTS DogsDB');
-    await connection.end();
+        await connection.query('CREATE DATABASE IF NOT EXISTS DogsDB');
+        await connection.end();
 
-    db = await mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        database: 'DogsDB'
-    });
+        db = await mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            database: 'DogsDB'
+        });
 
-    await db.execute(`
-        CREATE TABLE IF NOT EXISTS dogs (
-        dog_id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(255),
-        author VARCHAR(255)
-        )
-    `);
+        await db.execute(`
+            CREATE TABLE IF NOT EXISTS dogs (
+            dog_id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255),
+            author VARCHAR(255)
+            )
+        `);
 
-    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM ');
-    if (rows[0].count === 0) {
-      await db.execute(`
-        INSERT INTO books (title, author) VALUES
-        ('1984', 'George Orwell'),
-        ('To Kill a Mockingbird', 'Harper Lee'),
-        ('Brave New World', 'Aldous Huxley')
-      `);
-    }
+        const [rows] = await db.execute('SELECT COUNT(*) AS count FROM ');
+        if (rows[0].count === 0) {
+        await db.execute(`
+            INSERT INTO books (title, author) VALUES
+            ('1984', 'George Orwell'),
+            ('To Kill a Mockingbird', 'Harper Lee'),
+            ('Brave New World', 'Aldous Huxley')
+        `);
+        }
 
 })();
 
