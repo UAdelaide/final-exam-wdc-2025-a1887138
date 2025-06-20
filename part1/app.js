123607ba1d9f79c7dc03e2b89ae47a16473f033b
a1887138 <a1887138@adelaide.edu.au>
2025-06-20 04:18:44 +0000

    [automatic commit] 2025-06-20 04:18:44 UTC 101.115.236.113

--- part1/app.js
+++ part1/app.js
@@ -41,15 +41,14 @@ let db;
             password_hash VARCHAR(255) NOT NULL,
             role ENUM('owner', 'walker') NOT NULL,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-            )
-
+            );
             CREATE TABLE IF NOT EXISTS Dogs (
             dog_id INT AUTO_INCREMENT PRIMARY KEY,
             owner_id INT NOT NULL,
             name VARCHAR(50) NOT NULL,
             size ENUM('small', 'medium', 'large') NOT NULL,
             FOREIGN KEY (owner_id) REFERENCES Users(user_id)
-            )
+            );
         `);
 
         const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Dogs');
