4df8eb9180d9d0b69222fba5e08ed5d6810183c8
a1887138 <a1887138@adelaide.edu.au>
2025-06-20 03:39:34 +0000

    [automatic commit] 2025-06-20 03:39:34 UTC 101.115.236.113

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -7,9 +7,9 @@ router.get('/', function(req, res, next) {
 });
 
 // Route to return books as JSON
-router.get('/', async (req, res) => {
+router.get('dogs', async (req, res) => {
   try {
-    const [books] = await db.execute('SELECT * FROM books');
+    const [dogs] = await db.execute('SELECT * FROM dogs');
     res.json(books);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch books' });
