1ae4079421cd57ac90fbb9fb57123980c8e924fd
a1887138 <a1887138@adelaide.edu.au>
2025-06-20 06:20:42 +0000

    [automatic commit] 2025-06-20 06:20:42 UTC 101.115.236.113

--- starthere/app.js
+++ starthere/app.js
@@ -22,8 +22,8 @@ let db;
       password: '' // Set your MySQL root password
     });
 
-    // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS testdb');
+    // Reset the database
+    await connection.query('SOURCE part1/dogwalks.js');
     await connection.end();
 
     // Now connect to the created database
