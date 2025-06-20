a03d5b82f605e417f9e5df662e7adc365b61f472
a1887138 <a1887138@adelaide.edu.au>
2025-06-20 04:43:58 +0000

    [automatic commit] 2025-06-20 04:43:58 UTC 101.115.236.113

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const db = require('./db');
+const db = require('../db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
