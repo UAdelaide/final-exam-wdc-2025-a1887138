cd4759abfd991593556f2ec4c01a6da111bc880b
a1887138 <a1887138@adelaide.edu.au>
2025-06-20 06:37:57 +0000

    [automatic commit] 2025-06-20 06:37:57 UTC 101.115.236.113

--- part1/app.js
+++ part1/app.js
@@ -32,6 +32,40 @@ let db;
             database: 'DogWalkService'
         });
 
+                await db.execute(`
+            CREATE TABLE IF NOT EXISTS Users (
+            user_id INT AUTO_INCREMENT PRIMARY KEY,
+            username VARCHAR(50) UNIQUE NOT NULL,
+            email VARCHAR(100) UNIQUE NOT NULL,
+            password_hash VARCHAR(255) NOT NULL,
+            role ENUM('owner', 'walker') NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+        `);
+
+        await db.execute(`
+            CREATE TABLE IF NOT EXISTS Dogs (
+            dog_id INT AUTO_INCREMENT PRIMARY KEY,
+            owner_id INT NOT NULL,
+            name VARCHAR(50) NOT NULL,
+            size ENUM('small', 'medium', 'large') NOT NULL,
+            FOREIGN KEY (owner_id) REFERENCES Users(user_id)
+            );
+        `);
+
+        await db.execute(`
+            CREATE TABLE IF NOT EXISTS WalkRequests (
+            request_id INT AUTO_INCREMENT PRIMARY KEY,
+            dog_id INT NOT NULL,
+            requested_time DATETIME NOT NULL,
+            duration_minutes INT NOT NULL,
+            location VARCHAR(255) NOT NULL,
+            status ENUM('open', 'accepted', 'completed', 'cancelled') DEFAULT 'open',
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (dog_id) REFERENCES Dogs(dog_id)
+            );
+        `);
+
         var [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
         if (rows[0].count === 0) {
             await db.execute(`
