e44cce842569f928823d0ed103c96fd251b15670
a1887138 <a1887138@adelaide.edu.au>
2025-06-21 01:07:19 +0000

    [automatic commit] 2025-06-21 01:07:19 UTC 101.115.236.113

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,7 +49,7 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-    console.log(``);
+    console.log(`Rows[]`);
 
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
