b1443c86f0a2c6c7d40c5d3f5f767a17b4e778f5
a1887138 <a1887138@adelaide.edu.au>
2025-06-21 01:47:13 +0000

    [automatic commit] 2025-06-21 01:47:13 UTC 101.115.236.113

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -52,7 +52,7 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-    const role = rows[0].row;
+    const role = rows[0].role;
 
     console.log(`Role is ${role}`);
     console.log(`Rows is ${JSON.stringify(rows)}`);
