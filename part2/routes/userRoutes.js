5d7800aa8ce525ad3efbb77f006ab1d4e8dbc2f9
a1887138 <a1887138@adelaide.edu.au>
2025-06-21 05:20:22 +0000

    [automatic commit] 2025-06-21 05:20:22 UTC 101.115.236.113

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -33,7 +33,7 @@ router.get('/me', (req, res) => {
   if (!req.session.user) {
     return res.status(401).json({ error: 'Not logged in' });
   }
-  console.log(`responding!);
+  console.log(`responding with ${req.session.user}`);
   res.json(req.session.user);
 });
 
