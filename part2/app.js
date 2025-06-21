0d6dd685be4e4fc821291497efd7590c90d3113f
a1887138 <a1887138@adelaide.edu.au>
2025-06-21 02:01:47 +0000

    [automatic commit] 2025-06-21 02:01:47 UTC 101.115.236.113

--- part2/app.js
+++ part2/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(session({
-    
+    secret: "So secret even I'll forget this";
 }))
 
 // Routes
