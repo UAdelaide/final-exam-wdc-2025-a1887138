35259332063d58748717b4c088a11a06746aa848
a1887138 <a1887138@adelaide.edu.au>
2025-06-21 06:01:43 +0000

    [automatic commit] 2025-06-21 06:01:43 UTC 101.115.236.113

--- part2/app.js
+++ part2/app.js
@@ -20,7 +20,7 @@ const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 app.use((req, res, next) => {
-    // 
+    // We need SOME available paths. These are they.
     const publicPaths = ["", "/","/api/users/login"];
     const loggedInAs = req.session.role;
 
