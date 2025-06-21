7287fe3d8181c90990108e71910f3a8f2f5f0ac7
a1887138 <a1887138@adelaide.edu.au>
2025-06-21 02:00:25 +0000

    [automatic commit] 2025-06-21 02:00:25 UTC 101.115.236.113

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const session = require('express-session');
 const router = express.Router();
 const db = require('../models/db');
 
 
 router.all('/', async (req, res) => {
-  
+
 });
 
 // GET all walk requests (for walkers to view)
