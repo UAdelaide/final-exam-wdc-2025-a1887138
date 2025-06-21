739d0047b61f3a221991c21daadc2c1e2ce0bfd3
a1887138 <a1887138@adelaide.edu.au>
2025-06-21 01:01:53 +0000

    [automatic commit] 2025-06-21 01:01:53 UTC 101.115.236.113

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -39,9 +39,6 @@ router.get('/me', (req, res) => {
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
-  console.log(email);
-  console.log(password);
-
   try {
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
