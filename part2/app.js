b435494b919ca328ee8c3ee23b1ff157721d24c4
a1887138 <a1887138@adelaide.edu.au>
2025-06-21 00:32:38 +0000

    [automatic commit] 2025-06-21 00:32:38 UTC 101.115.236.113

--- part2/app.js
+++ part2/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.post('', async (req, res) => {
+app.post('', async (req, res, next) => {
     
 });
 
