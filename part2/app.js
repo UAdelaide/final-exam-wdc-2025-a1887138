50bc1d4f2f7a5cd9312827e17ecc9b7efa548f3f
a1887138 <a1887138@adelaide.edu.au>
2025-06-21 00:44:11 +0000

    [automatic commit] 2025-06-21 00:44:11 UTC 101.115.236.113

--- part2/app.js
+++ part2/app.js
@@ -13,6 +13,12 @@ app.use(express.static(path.join(__dirname, '/public')));
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
+app.post('/auth', async (req, res, next) => {
+    const username = req.body.username;
+    console.log(`Body is: ${JSON.stringify(req.body)}`);
+    console.log(`Username is: ${username}`);
+});
+
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
