8d20df18781372d89e86514922afa6836ae14930
a1887138 <a1887138@adelaide.edu.au>
2025-06-21 02:12:38 +0000

    [automatic commit] 2025-06-21 02:12:38 UTC 101.115.236.113

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -60,6 +60,7 @@ router.post('/login', async (req, res) => {
       res.status(500).json({ error: 'Server Error' });
     }
 
+    this.session.
     // res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
